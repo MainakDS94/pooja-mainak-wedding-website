88687fcef262d6132f95ccd6a357620ab6238e21
MainakDS94 <47924331+MainakDS94@users.noreply.github.com>
2023-03-19 13:13:56 +0100

    Customized
    Updated website

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Mainak and Pooja's Wedding Reception",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jun 25, 2023 18:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jun 25, 2023 23:59'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Raja Palace Banquet Hall, 1/2, Jessore Rd, opp. Emami City, NC Sen Avenue, K.B.Sarani, Golpark, Dum Dum, Kolkata, West Bengal 700080, India',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact at +91 98310 38305."
         }
     });
 
@@ -214,11 +214,8 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+
+            $.post('https://script.google.com/macros/s/AKfycbykm4fQKZNa3X_SgOwcM0a5ClVEHBO6X7MNkPEcfmGtdOpy8an0Ji2uYdbadPFrr-E5/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -226,7 +223,7 @@ $(document).ready(function () {
                     } else {
                         $('#alert-wrapper').html('');
                         $('#rsvp-modal').modal('show');
-                    }
+                    
                 })
                 .fail(function (data) {
                     console.log(data);
@@ -241,7 +238,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 22.628414, lng: 88.421628};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
